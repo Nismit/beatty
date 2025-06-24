3423d0a24d2ebfd774b580112227a0e10a6fb291
Nismit <22581499+Nismit@users.noreply.github.com>
2025-06-23 22:16:15 -0700

    .

---
+++ src/sw.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = 'beatty-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './main.js',
+  './AppState.js',
+  './audio.js',
+  './audio-analyzer.js',
+  './audio-worklet.js',
+  './SoundGL.js',
+  './VisualGL.js',
+  './editor.js',
+  './shader-templates.js',
+  './StatusManager.js',
+  './storage.js',
+  './consts.js',
+  './utils.js',
+  './styles.css',
+  './manifest.json'
+];
+
+// Install event - cache resources
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event - serve from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
